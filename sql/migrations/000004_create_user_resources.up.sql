f05acf4ee23bd0356eabb2e64535ac7e4f92e446
queirozdaniel <danielqueirozak77@gmail.com>
2024-02-06 21:35:34 -0300

    fix: adjusts in migration

--- sql/migrations/000004_create_user_resources.up.sql
+++ sql/migrations/000004_create_user_resources.up.sql
@@ -1,8 +1,7 @@
 CREATE TABLE users_resources (
+    id uuid primary key,
     user_id uuid,
     role_resource_id uuid,
     foreign key(user_id) references users(id),
     foreign key(role_resource_id) references roles_resources(id)
 );
-
-ALTER TABLE users_resources ADD CONSTRAINT "ID_PKEY" KEY (user_id, role_resource_id);
